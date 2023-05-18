6bf73e962d19be3c484b96d8d41b53242bc150a9
aiden21c <aiden21con@gmail.com>
2023-05-18 23:24:12 +1000

    Updated some scripts used

--- register_file/python_scripts/Untitled-2.py
+++
@@ -1,7 +0,0 @@
-def main():
-    with open("file_mux.txt", "w") as file:
-        for x in range(32):
-            file.write("multi_bit_multiplexer_2way #(8) reg0_in_Mux (.A(reg" + str(x) + "_out), .B(WD), .S(RegWrite & reg" + str(x) + "_sel), .out(reg" + str(x) + "_in));" "\n")
-            
-if __name__ == "__main__":
-    main()
